60fe95e0e2609560435b9a72981c8d9009c629fe
Esteb4ncd <davidecruzd10@gmail.com>
2024-09-20 15:54:13 -0700

    initialize stables

---
+++ stables.js
@@ -0,0 +1,11 @@
+let horseName = "Aguacate";
+console.log(horseName);
+horseName = "Hello";
+console.log(horseName)
+
+let horseAge = 5;
+console.log(horseAge);
+
+let isHorseInside = true;
+console.log(isHorseInside)
+  
