17d85a02b90855806537a3abe188137bd273a099
Esteb4ncd <davidecruzd10@gmail.com>
2024-09-27 15:21:25 -0700

    adding info to stables

--- stables.js
+++ stables.js
@@ -1,6 +1,7 @@
+//LESSON-3 code
 let horseName = "Aguacate";
 console.log(horseName);
-horseName = "Hello";
+horseName = "Aguacate";
 console.log(horseName)
 
 let horseAge = 5;
@@ -9,3 +10,16 @@ console.log(horseAge);
 let isHorseInside = true;
 console.log(isHorseInside)
   
+//LESSON-4 code
+console.log(typeof fakeVariable)
+const STABLE_MONTHLY_FEE = 18; //it is good pracice to write constants in ALL CAPS
+console.log(STABLE_MONTHLY_FEE); 
+
+console.log(STABLE_MONTHLY_FEE * 3);
+console.log("hi" + " there")
+console.log("hi" + 2)//this is type coercion
+console.log(horseAge / 34) //remember horseAge=5
+console.log(true + 4)//true=1 and false=0
+console.log(STABLE_MONTHLY_FEE + " is the stable's monthly fee, and for 3 months it would be " + (STABLE_MONTHLY_FEE * 3 - 20));//
+let boardPrice = 230
+console.log(`My horse's name is "${horseName}" and it costs $${boardPrice} to ride him"`)
