70e52a3ee34c240312636b40c56afbe5a5c5788e
Esteb4ncd <davidecruzd10@gmail.com>
2024-09-27 16:20:55 -0700

    lab sep27th

--- stables.js
+++ stables.js
@@ -12,7 +12,7 @@ console.log(isHorseInside)
   
 //LESSON-4 code
 console.log(typeof fakeVariable)
-const STABLE_MONTHLY_FEE = 18; //it is good pracice to write constants in ALL CAPS
+const STABLE_MONTHLY_FEE = 2560; //it is good pracice to write constants in ALL CAPS
 console.log(STABLE_MONTHLY_FEE); 
 
 console.log(STABLE_MONTHLY_FEE * 3);
@@ -20,6 +20,11 @@ console.log("hi" + " there")
 console.log("hi" + 2)//this is type coercion
 console.log(horseAge / 34) //remember horseAge=5
 console.log(true + 4)//true=1 and false=0
-console.log(STABLE_MONTHLY_FEE + " is the stable's monthly fee, and for 3 months it would be " + (STABLE_MONTHLY_FEE * 3 - 20));//
+console.log(STABLE_MONTHLY_FEE + " is the stable's monthly fee, and for 3 months it would be " + (STABLE_MONTHLY_FEE * 3 - 20))
 let boardPrice = 230
 console.log(`My horse's name is "${horseName}" and it costs $${boardPrice} to ride him"`)
+
+//LAB LESSON 4
+let horseNickname = "Aguita"
+console.log(`My horse's name is "${horseName}" and his nickname is "${horseNickname}"`);
+console.log(`The 3 month stay at Esteban's stables gives you a 10% discount, so you get to save ` + "$" + ((STABLE_MONTHLY_FEE * 3)*0.1));
